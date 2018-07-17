2eaf2192ecece93b95f526c625bc819247022216
Jane <Jane@Johny5s-iMac.local>
2018-07-17 14:08:00 -0500

    update

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Digital Innovation Lab</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default card;
